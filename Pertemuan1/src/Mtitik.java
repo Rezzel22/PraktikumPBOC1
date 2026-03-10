e96be5044aa87c5cc21c928757a7370f3d91cf21
Imam Alfarezel <imamalfarezel20@gmail.com>
2026-03-10 23:45:15 +0700

    Revisi Pertemuan1

--- Pertemuan1/src/Mtitik.java
+++ Pertemuan1/src/Mtitik.java
@@ -5,11 +5,23 @@
 
 public class Mtitik {
     public static void main(String[] args) {
+
+        // Membuat objek titik T1 menggunakan konstruktor default
         Titik T1 = new Titik();
+
+        // Mengatur nilai absis (x) dari titik menjadi 3
         T1.setabsis(3);
+
+        // Mengatur nilai ordinat (y) dari titik menjadi 4
         T1.setordinat(4);
+
+        // Menampilkan koordinat titik T1
         T1.printTitik();
+
+        // Menggeser titik T1 sejauh 3 pada sumbu x dan 4 pada sumbu y
         T1.geser(3,4);
+
+        // Menampilkan kembali posisi titik setelah digeser
         T1.printTitik();
     }
 }
--- Pertemuan1/src/Titik.java
+++ Pertemuan1/src/Titik.java
@@ -1,38 +1,57 @@
+/*Nama File     : MataKuliah.java
+ *Deskripsi     : Berisi atribut dan method dalam class MataKuliah
+ *Pembuat       : Imam Alfarezel(24060124120028)
+ *Tanggal       : 19/02/2026
+ *Lab           : C1*/
+
 public class Titik {
     /*Atribut*/
     double absis;
     double ordinat;
 
-    /*Method */
+    /*Kontruktor*/ 
+
+    // Konstruktor default untuk membuat titik dengan koordinat awal (0,0)
     Titik(){
         absis = 0;
         ordinat = 0;
     }
 
+    /*Selektor*/
+
+    // Method untuk mengambil nilai absis (koordinat x)
     double getabsis(){
         return absis;
     }
 
+    // Method untuk mengambil nilai ordinat (koordinat y)
     double getordinat(){
         return ordinat;
     }
 
+    /*Mutator */
+
+    // Method untuk mengubah nilai absis (koordinat x)
     void setabsis(double x){
         absis = x;
     }
 
+    // Method untuk mengubah nilai ordinat (koordinat y)
     void setordinat(double y){
         ordinat = y;
     }
 
+    /*Method Tambahan */
+    
+    // Method untuk menggeser posisi titik sejauh x pada absis dan y pada ordinat
     void geser(double x, double y){
         absis = absis + x;
         ordinat = ordinat + y;
     }
 
+    /*Method untuk menampilkan titik */
     void printTitik(){
         System.out.println("Titik(" + absis + "," + ordinat + ")");
     }
 
-    
 }
